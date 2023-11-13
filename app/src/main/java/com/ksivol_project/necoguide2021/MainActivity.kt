81bf9d613b9c26ad8c2ce87314eb8737e6cbc8a8
Ksivol <surkin-ant@mail.com>
2023-11-13 15:47:50 +0400

    lesson 1-5

--- app/src/main/java/com/ksivol_project/necoguide2021/MainActivity.kt
+++ app/src/main/java/com/ksivol_project/necoguide2021/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ksivol_project.necoguide2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun onClickTest(view: View) {
+        val tvTest = findViewById<TextView>(R.id.tvTest)
+        tvTest.text = "privet"
+    }
+
 }
