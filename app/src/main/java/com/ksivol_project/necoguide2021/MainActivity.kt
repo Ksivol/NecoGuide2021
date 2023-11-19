5cbb5501f8b5e9ceeba3d7469812c5624ee12814
Ksivol <surkin-ant@mail.com>
2023-11-19 19:33:07 +0400

    lesson 6-17

--- app/src/main/java/com/ksivol_project/necoguide2021/MainActivity.kt
+++ app/src/main/java/com/ksivol_project/necoguide2021/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.ksivol_project.necoguide2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import com.ksivol_project.necoguide2021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
 
     }
 
-    fun onClickTest(view: View) {
-        val tvTest = findViewById<TextView>(R.id.tvTest)
-        tvTest.text = "privet"
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && requestCode == RESULT_OK && data != null){
+
+            bindingClass.tvMessageMain.text = data.getStringExtra("key2")
+
+        }
+    }
+
+    fun onClickTest1(view: View){
+
+        val i = Intent(this, TestActivity1::class.java)
+        i.putExtra("key","Как тебя зовут?")
+        startActivityForResult(i, 100)
+
     }
 
 }
